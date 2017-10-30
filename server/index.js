76b7cb4e60d86e7ebb2e854f7cf59fba607cfd8a
Ilyas Abdighni <udunig@gmail.com>
2017-10-30 17:05:09 +0100

    keys.js is added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'There' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 3000;
 
