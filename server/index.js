a630cfba682ca91a2b869b51df1f7b9fcac0f82f
Ilyas Abdighni <udunig@gmail.com>
2017-11-03 09:46:34 +0100

    fixed

--- server/index.js
+++ server/index.js
@@ -28,13 +28,14 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
   // Express will serve up production assets
   // like our main.js file, or main.css file
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
   // Express will serve up the index.html file
   //if it doesn't recognise the route
-  const path = require('path');
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
